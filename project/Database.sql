b54e1e072736a0440b3af7c9b82d3f6c80cafc5e
Edward Wills <ewills1@sheffield.ac.uk>
2023-11-13 17:39:18 +0000

    Updated db

--- project/Database.sql
+++ project/Database.sql
@@ -1,11 +1,14 @@
 CREATE TABLE Product ( 
     productID INT NOT NULL PRIMARY KEY,
-    name STRING,
+    name VARCHAR (100),
     price INT,
     quantity INT,
-    FOREIGN KEY (locomotiveID) REFERENCES Locomotive(locomotiveID),
-    FOREIGN KEY (rollingStockID) REFERENCES RollingStock(rollingStockID),
-    FOREIGN KEY (controllerID) REFERENCES Controller(controllerID)
+    locomotiveID INT,
+    rollingStockID INT,
+    controllerID INT,
+    FOREIGN KEY (locomotiveID) REFERENCES Locomotive(productID),
+    FOREIGN KEY (rollingStockID) REFERENCES RollingStock(productID),
+    FOREIGN KEY (controllerID) REFERENCES Controller(productID)
 );
 
 CREATE TABLE OrderLine (
@@ -13,59 +16,65 @@ CREATE TABLE OrderLine (
     productID INT,
     orderLineNumber INT,
     productQuantity INT, 
-    orderLineCost MONEY,
-    PRIMARY FOREIGN KEY (orderID, productID),
-    FOREIGN KEY (orderID) REFERENCES Order (orderID),
+    orderLineCost DECIMAL (8,2),
+    PRIMARY KEY (orderID, productID),
+    FOREIGN KEY (orderID) REFERENCES `Order` (orderID),
     FOREIGN KEY (productID) REFERENCES Product (productID)
 );
 
-CREATE TABLE Order (
+CREATE TABLE `Order` (
     orderID INT NOT NULL PRIMARY KEY,
     date DATE,
-    totalCost MONEY,
-    status STATUS
+    totalCost DECIMAL (8,2),
+    status BOOL
 );
 
 CREATE TABLE User (
-    userID NOT NULL PRIMARY KEY,
-    forename STRING,
-    surname STRING,
-    bankCardName STRING,
-    cardHolderName STRING,
-    expiryDate STRING,
+    userID INT NOT NULL PRIMARY KEY,
+    forename VARCHAR (100),
+    surname VARCHAR (100),
+    bankCardName VARCHAR (100),
+    cardHolderName VARCHAR (100),
+    expiryDate VARCHAR (100),
     securityCode INT,
-    userType TYPE,
-    PRIMARY FOREIGN KEY (houseID) REFERENCES Address (houseID)
+    userType VARCHAR (100),
+    PRIMARY KEY (houseID),
+	FOREIGN KEY (houseID) REFERENCES Address (houseID)
 );
 
 CREATE TABLE Address (
-    houseID NOT NULL PRIMARY KEY,
-    streetName STRING,
-    cityName STRING,
-    postcode STRING
+    houseID INT NOT NULL PRIMARY KEY,
+    streetName VARCHAR (100),
+    cityName VARCHAR (100),
+    postcode VARCHAR (100)
 );
 
 CREATE TABLE Locomotive (
-    PRIMARY FOREIGN KEY (productID) REFERENCES Product (productID),
+	PRIMARY KEY (productID),
+    FOREIGN KEY (productID) REFERENCES Product (productID),
     FOREIGN KEY (trainSetID) REFERENCES TrainSet(trainSetID)
 );
 
 CREATE TABLE RollingStock (
-    PRIMARY FOREIGN KEY (productID) REFERENCES Product(productID),
+	PRIMARY KEY (productID),
+	FOREIGN KEY (productID) REFERENCES Product(productID),
     FOREIGN KEY (trainSetID) REFERENCES TrainSet (trainSetID)
 );
 
 CREATE TABLE Controller (
-    PRIMARY FOREIGN KEY (productID) REFERENCES Product(productID)
+	PRIMARY KEY (productID),
+    FOREIGN KEY (productID) REFERENCES Product(productID)
 );
 
 CREATE TABLE TrainSet (
-    trainSetID NOT NULL PRIMARY KEY,
-    PRIMARY FOREIGN KEY (productID) REFERENCES Product (productID),
+    trainSetID INT,
+    PRIMARY KEY (productID, trainSetID),
+    FOREIGN KEY (productID) REFERENCES Product (productID),
     FOREIGN KEY (trainPackProductID) REFERENCES TrackPack (productID),
-    FOREIGN KEY (controllerProductID)
+    FOREIGN KEY (controllerProductID) REFERENCES Controller (productID)
 );
 
 CREATE TABLE TrackPack (
-    PRIMARY FOREIGN KEY (productID) REFERENCES Product (productID)
+	PRIMARY KEY (productID),
+    FOREIGN KEY (productID) REFERENCES Product (productID)
 );
