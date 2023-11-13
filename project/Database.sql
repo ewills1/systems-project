c8e9423eaac6c6add1b80b41237d3b6f9c29aa2c
Edward Wills <ewills1@sheffield.ac.uk>
2023-11-13 11:37:39 +0000

    Updated database to finish tables

--- project/Database.sql
+++ project/Database.sql
@@ -16,3 +16,54 @@ CREATE TABLE OrderLine (
     orderLineCost MONEY,
     PRIMARY FOREIGN KEY (orderID, productID)
 );
+
+CREATE TABLE Order (
+    orderID INT NOT NULL PRIMARY KEY,
+    date Date,
+    totalCost Money,
+    status Status
+);
+
+CREATE TABLE User (
+    userID NOT NULL PRIMARY KEY,
+    forename String,
+    surname String,
+    bankCardName String,
+    cardHolderName String,
+    expiryDate String,
+    securityCode Int,
+    userType Type,
+    PRIMARY FOREIGN KEY (houseID)
+);
+
+CREATE TABLE Address (
+    houseID NOT NULL PRIMARY KEY,
+    streetName String,
+    cityName String,
+    postcode String
+);
+
+CREATE TABLE Locomotive (
+    PRIMARY FOREIGN KEY (productID)
+    FOREIGN KEY (trainSetID)
+);
+
+CREATE TABLE RollingStock (
+    PRIMARY FOREIGN KEY (productID)
+    FOREIGN KEY (trainSetID)
+);
+
+CREATE TABLE Controller (
+    PRIMARY FOREIGN KEY (productID)
+);
+
+CREATE TABLE TrainSet (
+    trainSetID NOT NULL PRIMARY KEY
+    PRIMARY FOREIGN KEY (productID)
+    FOREIGN KEY (trainPackProductID)
+    FOREIGN KEY (controllerProductID)
+);
+
+CREATE TABLE TrackPack (
+    PRIMARY FOREIGN KEY (productID)
+);
