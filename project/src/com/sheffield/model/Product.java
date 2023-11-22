566eb15f8ec548f2b31015b9693d3da203035a2d
Harry Hodges <hhodges2@sheffield.ac.uk>
2023-11-22 15:49:41 +0000

    completed product class and added locomotive class

--- project/src/com/sheffield/model/Product.java
+++ project/src/com/sheffield/model/Product.java
@@ -1,32 +1,36 @@
 package com.sheffield.model;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class Product {
-    private String code;
+    private String productCode;
     private String name;
     private String brandName;
     private int quantity;
     private BigDecimal price;
 
 
-    // Constructor to initialize a Book object with its attributes
-    public Product(String name, String code, String brandName, int quantity, BigDecimal price) {
-        this.setCode(code);
+    // Constructor to initialize a Product object with its attributes
+    public Product(String name, String productCode, String brandName, int quantity, BigDecimal price) {
+        this.setProductCode(productCode);
         this.setName(name);
         this.setBrandName(brandName);
         this.setQuantity(quantity);
         this.setPrice(price);
     }
 
-    // Getter and setter methods for the Code attribute with validation
-    public String getCode() {
-        return code;
+    // Getter and setter methods for the productCode attribute with validation
+    public String getProductCode() {
+        return productCode;
     }
 
-    public void setCode(String code) {
+    public void setProductCode(String productCode) {
         if (isValidCode(code)) {
-            this.code = code;
+            this.code = productCode;
         } else {
             throw new IllegalArgumentException("Product code is not valid.");
         }
@@ -85,9 +89,8 @@ public class Product {
     }
 
     // Private validation methods for each attribute
-    private boolean isValidCode(String code) {
-        // Product code is a single letter followed by a 3 to 5 digit serial number
-        return code != null && code.length() >= 4 && code.length() <= 6;
+    private boolean isValidProductCode(String productCode) {
+        return productCode != null && productCode.length() <= 100;
     }
 
     private boolean isValidName(String name) {
@@ -102,11 +105,6 @@ public class Product {
         return quantity >= 0;
     }
 
-    // private boolean isValidCopiesAvailable(int copiesAvailable) {
-    //     // Implement copies available validation logic here (e.g., non-negative)
-    //     return copiesAvailable >= 0;
-    // }
-
     private boolean isValidPrice(BigDecimal price) {
         // price must be non-negative
         return price != null && price.compareTo(BigDecimal.ZERO) >= 0;
@@ -116,11 +114,130 @@ public class Product {
     @Override
     public String toString() {
         return "{ " +
-            " code='" + getCode() + "'" +
+            " productCode='" + getProductCode() + "'" +
             ", name='" + getName() + "'" +
             ", brandName='" + getBrandName() + "'" +
             ", quantity='" + getQuantity() + "'" +
             ", price='" + getPrice() + "'" +
             " }";
+    }
+
+    // Insert a new product into the database
+    public void insertProduct(Product newProduct, Connection connection) throws SQLException {
+        try {
+            // Create an SQL INSERT statement
+            String insertSQL = "INSERT INTO Products (productCode, name, brand_name,"+
+            "quantity, price) VALUES (?, ?, ?, ?, ?, ?)";
+
+            // Prepare and execute the INSERT statement
+            PreparedStatement preparedStatement = connection.prepareStatement(insertSQL);
+            preparedStatement.setString(1, newProduct.getProductCode());
+            preparedStatement.setString(2, newProduct.getName());
+            preparedStatement.setString(3, newProduct.getBrandName());
+            preparedStatement.setInt(4, newProduct.getQuantity());
+            preparedStatement.setBigDecimal(5, newProduct.getPrice());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println(rowsAffected + " row(s) inserted successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e; // Re-throw the exception to signal an error.
+        }
+    }
+
+    // Get all products from the database
+    public void getAllProducts(Connection connection) throws SQLException {
+        try {
+            String selectSQL = "SELECT * FROM Products";
+            PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("<=================== GET ALL PRODUCTS ====================>");
+            while (resultSet.next()) {
+                // Print each product's information in the specified format
+                System.out.println("{" +
+                        "productCode='" + resultSet.getString("productCode") + "'" +
+                        ", name='" + resultSet.getString("name") + "'" +
+                        ", brandName='" + resultSet.getString("brand_name") + "'" +
+                        ", quantity='" + resultSet.getInt("quantity") + "'" +
+                        ", price='" + resultSet.getDouble("price") + "'" +
+                        "}");
+            }
+            System.out.println("<======================================================>");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;// Re-throw the exception to signal an error.
+        }
+    }
+
+    // Get a product by it's productCode
+    public void getProductByCode(String productCode, Connection connection) throws SQLException {
+        try {
+            String selectSQL = "SELECT * FROM Products WHERE productCode=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+            preparedStatement.setString(1, productCode);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("<==================== PRODUCT BY CODE =====================>");
+            if (resultSet.next()) {
+                System.out.println("{" +
+                        "productCode='" + resultSet.getString("productCode") + "'" +
+                        ", name='" + resultSet.getString("name") + "'" +
+                        ", brandName='" + resultSet.getString("brand_name") + "'" +
+                        ", quantity='" + resultSet.getInt("quantity") + "'" +
+                        ", price='" + resultSet.getDouble("price") + "'" +
+                        "}");
+            } else {
+                System.out.println("Product with productCode " + productCode + " not found.");
+            }
+            System.out.println("<=======================================================>");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;// Re-throw the exception to signal an error.
+        }
+    }
+
+    // Update an existing product in the database
+    public void updateProduct(Product newProduct, Connection connection) throws SQLException {
+        try {
+            String updateSQL = "UPDATE Products SET name=?, brandName=?,"+
+            "quantity=?, price=? WHERE productCode=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateSQL);
+
+            preparedStatement.setString(1, newProduct.getName());
+            preparedStatement.setString(2, newProduct.getBrandName());
+            preparedStatement.setInt(3, newProduct.getQuantity());
+            preparedStatement.setBigDecimal(4, newProduct.getPrice());
+            preparedStatement.setString(5, newProduct.getProductCode());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println(rowsAffected + " row(s) updated successfully.");
+            } else {
+                System.out.println("No rows were updated for productCode: " + newProduct.getProductCode());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;// Re-throw the exception to signal an error.
+        }
+    }
+
+    // Delete a product from the database by productCode
+    public void deleteProduct(String productCode, Connection connection) throws SQLException {
+        try {
+            String deleteSQL = "DELETE FROM Product WHERE productCode=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL);
+            preparedStatement.setString(1, productCode);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println(rowsAffected + " row(s) deleted successfully.");
+            } else {
+                System.out.println("No rows were deleted for the product code: " + productCode);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;// Re-throw the exception to signal an error.
+        }
     }
 }
