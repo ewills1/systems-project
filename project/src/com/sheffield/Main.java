a800226713b170b2444e275131b5c24375ce6e77
Syakir <msbhasram1@sheffield.ac.uk>
2023-11-22 21:36:10 +0000

    Merge branch 'main' of https://github.com/ewills1/systems-project Connect with database
    # Conflicts:
    #	project/src/com/sheffield/Main.java

--- project/src/com/sheffield/Main.java
+++ project/src/com/sheffield/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
         // Create an instance of DatabaseConnectionHandler for managing database connections
         DatabaseConnectionHandler databaseConnectionHandler = new DatabaseConnectionHandler();
-<<<<<<< HEAD
 
         // Execute the Swing GUI application on the Event Dispatch Thread
         SwingUtilities.invokeLater(() -> {
@@ -31,7 +30,5 @@ public class Main {
             }
         });
 
-=======
->>>>>>> 334a6e66264fa3ea3f880c3ffa51f761862b37ba
     }
 }
