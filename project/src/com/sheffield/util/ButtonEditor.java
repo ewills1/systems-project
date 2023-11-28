a7422ddc49f3fbe285e1b3b2b97ad396d11e007c
Afiq Ismail <mabismail1@sheffield.ac.uk>
2023-11-28 12:03:21 +0000

    Code Modification: ProductListingScreen using product data from database

--- project/src/com/sheffield/util/ButtonEditor.java
+++ project/src/com/sheffield/util/ButtonEditor.java
@@ -13,6 +13,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import com.sheffield.views.ItemFormScreen;
+import com.sheffield.views.ProductListingScreen;
 
 // Custom cell editor to handle button clicks
 public class ButtonEditor extends DefaultCellEditor {
@@ -22,6 +23,8 @@ public class ButtonEditor extends DefaultCellEditor {
     private JTable table;
     private Connection connection;
     private String storedId;
+    private String storedProductName;
+    private int quantity;
 
     public ButtonEditor(JTextField textField, JTable table, Connection connection) {
         super(textField);
@@ -33,6 +36,8 @@ public class ButtonEditor extends DefaultCellEditor {
             int row = this.table.getEditingRow(); // Get the row index of the clicked button
             int column = 1; // Get the column index of the clicked button
             storedId = (String) table.getValueAt(row, column);
+            storedProductName = (String) table.getValueAt(row, column+1);
+            quantity = (int) table.getValueAt(row, column + 4);
             // Perform different actions based on row and column indices
             fireEditingStopped();
         });
@@ -48,19 +53,36 @@ public class ButtonEditor extends DefaultCellEditor {
     public Object getCellEditorValue() {
         if (isPushed) {
             // Perform the action when the button is clicked
-            JOptionPane.showMessageDialog(button, "Editing: " + storedId);
+            if (this.label == "Update/Delete") {
+                JOptionPane.showMessageDialog(button, "Editing: " + storedId);
+                // Close the current screen (frame)
+                Window window = SwingUtilities.windowForComponent(button);
+                if (window instanceof JFrame) {
+                    window.dispose();
+                }
 
-            // Close the current screen (frame)
-            Window window = SwingUtilities.windowForComponent(button);
-            if (window instanceof JFrame) {
-                window.dispose();
+                // Open another screen (frame)
+                JFrame newFrame = new ItemFormScreen(connection, storedId);
+                newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Change to your desired close operation
+                // Configure the new frame: set size, add components, etc.
+                newFrame.setVisible(true);
+            } else if (this.label == "+ OrderLine") {
+                JFrame frame = new JFrame();
+                String inputQuantity = JOptionPane.showInputDialog(frame, "Enter quantity for "+ this.storedProductName + " :");
+                
+                if (Integer.parseInt(inputQuantity) > quantity) {
+                    JOptionPane.showMessageDialog(button, "Input too large");
+                } else {
+                    JOptionPane.showMessageDialog(button, "Product added  to OrderLine");
+                    // Window window = SwingUtilities.windowForComponent(button);
+                    // if (window instanceof JFrame) {
+                    //     window.dispose();
+                    // }
+                    
+                    // JFrame newFrame = new ProductListingScreen(connection, "");
+                    // newFrame.setVisible(true);
+                }
             }
-
-            // Open another screen (frame)
-            JFrame newFrame = new ItemFormScreen(connection, storedId);
-            newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Change to your desired close operation
-            // Configure the new frame: set size, add components, etc.
-            newFrame.setVisible(true);
         }
         isPushed = false;
         return label;
