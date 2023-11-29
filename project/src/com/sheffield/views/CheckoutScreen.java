cc8fdccc106d589b2717bba5530ab71a9a3c09ba
Afiq Ismail <mabismail1@sheffield.ac.uk>
2023-11-29 20:41:55 +0000

    Code Modification: Modify checkout screen

--- project/src/com/sheffield/views/CheckoutScreen.java
+++ project/src/com/sheffield/views/CheckoutScreen.java
@@ -1,29 +1,42 @@
 package com.sheffield.views;
+import com.sheffield.model.CurrentUserManager;
 /**
  * @author afiq_ismail
  */
 import com.sheffield.model.DatabaseOperations;
+import com.sheffield.model.Order;
+import com.sheffield.model.Status;
+import com.sheffield.util.ButtonEditor;
+import com.sheffield.util.ButtonRenderer;
 
 import java.awt.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Vector;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class CheckoutScreen extends JFrame {
     /**
      * Needed for serialisation
      */
     private static final long serialVersionUID = 1L;
+    DatabaseOperations databaseOperations = new DatabaseOperations();
 
     // Variables declaration                 
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JList<String> jList1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel orderLinePanel;
+    private javax.swing.JTabbedPane orderLineTab;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration
     
     /**
@@ -51,26 +64,30 @@ public class CheckoutScreen extends JFrame {
      */
     private void initComponents(Connection connection, String id) {
 
+        
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
         jButton1 = new javax.swing.JButton();
+        orderLineTab = new javax.swing.JTabbedPane();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
-        DatabaseOperations databaseOperations = new DatabaseOperations();
+        orderLinePanel = createPanel(connection);
+        jTextField1 = new javax.swing.JTextField();
+
+        orderLineTab.addTab("Order Line", orderLinePanel);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Order Line Screen");
-        setMinimumSize(new java.awt.Dimension(623, 574));
+        setTitle("Order Checkout Screen");
+        setMinimumSize(new java.awt.Dimension(1216, 636));
         setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setText("Order Summary");
 
-        jButton2.setText("Order Line");
+        jButton2.setText("Main Screen");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 goToOrderLine(connection, id, evt);
@@ -82,9 +99,9 @@ public class CheckoutScreen extends JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(60, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addGap(108, 108, 108)
+                .addGap(137, 400, 400)
                 .addComponent(jButton2)
                 .addGap(24, 24, 24))
         );
@@ -97,13 +114,6 @@ public class CheckoutScreen extends JFrame {
                     .addComponent(jLabel1))
                 .addContainerGap(28, Short.MAX_VALUE))
         );
-
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(jList1);
 
         jButton1.setText("Place Order");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -128,35 +138,51 @@ public class CheckoutScreen extends JFrame {
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setText("Total: £ XXXX");
+        jLabel2.setText("Messages: ");
+        jLabel2.setVisible(false);
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel3.setText("Total: £ XXXX");
+
+        jTextField1.setEditable(false);
+        jTextField1.setText("Your order line is empty. Add item now.");
+        jTextField1.setVisible(false);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel2)
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addGap(123, 123, 123)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 362, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addGap(250, 250, 250)
-                            .addComponent(jButton1))))
-                .addContainerGap(126, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(245, 1000, 1000)
+                        .addComponent(jLabel3))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(245, 500, 500)
+                        .addComponent(jButton1))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(orderLineTab, javax.swing.GroupLayout.PREFERRED_SIZE, 1100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField1)))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(44, 44, 44)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(orderLineTab, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                .addComponent(jLabel3)
                 .addComponent(jButton1)
-                .addGap(41, 41, 41))
+                .addGap(29, 29, 29))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -199,5 +225,167 @@ public class CheckoutScreen extends JFrame {
     private void goToProfile(Connection connection, String id, java.awt.event.ActionEvent evt) {
         dispose();
         new ProfileScreen(connection, id);
+    }
+
+    private JPanel createPanel(Connection connection) {
+        JPanel productPanel = new javax.swing.JPanel();
+        JTable userTable = new javax.swing.JTable();
+        JScrollPane jScrollPane2 = new javax.swing.JScrollPane();
+
+        GroupLayout productPanelLayout = new GroupLayout(productPanel);
+        
+        productPanel.setLayout(productPanelLayout);
+        productPanelLayout.setHorizontalGroup(
+            productPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(productPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 1186, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        productPanelLayout.setVerticalGroup(
+            productPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(productPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE))
+        );
+
+        // try
+
+        try {
+            ResultSet orderIDOrderLineResultSet;
+            DefaultTableModel countModel = new DefaultTableModel();
+            countModel.addColumn("No.");
+            DefaultTableModel userModel = new DefaultTableModel();
+            DefaultTableModel combinedTableModel = new DefaultTableModel();
+            // // Create a DefaultTableModel with a JButton column
+            DefaultTableModel actionModel = new DefaultTableModel() {
+                @Override
+                public Class<?> getColumnClass(int columnIndex) {
+                    return columnIndex == 2 ? JButton.class : Object.class; // Column index 2 contains buttons
+                }
+            };
+
+            // constructing defined orderID
+            String userIDFirst2Char = CurrentUserManager.getCurrentUser().getUserID();
+            String orderID = userIDFirst2Char.substring(0, Math.min(userIDFirst2Char.length(), 2)).toUpperCase();
+            int userOrderCount = databaseOperations.countUserOrder(CurrentUserManager.getCurrentUser().getUserID(), connection);
+            orderID = orderID + userOrderCount;
+
+            Order order = databaseOperations.getOrderModel(orderID, connection);
+            
+            if (order.getStatus().equals(Status.PENDING) && (databaseOperations.countOrderIDOrderLine(orderID, connection)) != 0) {
+                orderIDOrderLineResultSet = databaseOperations.getAllOrderIDOrderLineData(connection, orderID);
+                userModel = buildTableModel(orderIDOrderLineResultSet);
+                for (int i = 0; i < databaseOperations.countOrderIDOrderLine(orderID, connection); i++) {
+                    countModel.addRow(new Object[]{i+1});
+                }
+                combinedTableModel = countModel;
+                combinedTableModel = combineTableModels(countModel, userModel);
+                actionModel.addColumn("Action");
+                for (int i = 0; i < databaseOperations.countOrderIDOrderLine(orderID, connection); i++) {
+                    actionModel.addRow(new Object[]{ "Remove"});
+                }
+                
+                combinedTableModel = combineTableModels(combinedTableModel, actionModel);
+                userTable.setModel(combinedTableModel);
+                userTable.getColumnModel().getColumn(combinedTableModel.getColumnCount() - 1).setCellRenderer(new ButtonRenderer());
+                userTable.getColumnModel().getColumn(combinedTableModel.getColumnCount() - 1).setCellEditor(new ButtonEditor(new JTextField(), userTable, connection));
+                userTable.setColumnSelectionAllowed(true);
+                jScrollPane2.setViewportView(userTable);
+            } else {
+                userTable.setModel(new javax.swing.table.DefaultTableModel(
+                    new Object [][] {
+                    },
+                    new String [] {
+                        "No.", " productCode", "productQuantity", "orderLineCost", "Action"
+                    }
+                ) {
+                    boolean[] canEdit = new boolean [] {
+                        false, false, false, false, false
+                    };
+
+                    public boolean isCellEditable(int rowIndex, int columnIndex) {
+                        return canEdit [columnIndex];
+                    }
+                });
+            }
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return productPanel;
+    }
+
+    public DefaultTableModel buildTableModel(ResultSet resultSet) throws SQLException {
+        java.sql.ResultSetMetaData metaData = resultSet.getMetaData();
+
+        // Get column count
+        int columnCount = metaData.getColumnCount();
+
+        // Create a DefaultTableModel to hold the data
+        DefaultTableModel tableModel = new DefaultTableModel();
+
+        // Add column names to the table model
+        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+            tableModel.addColumn(metaData.getColumnLabel(columnIndex));
+        }
+
+        // Add row data to the table model
+        while (resultSet.next()) {
+            Object[] rowData = new Object[columnCount];
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                rowData[columnIndex - 1] = resultSet.getObject(columnIndex);
+            }
+            tableModel.addRow(rowData);
+        }
+
+        return tableModel;
+    }
+
+    // Method to combine two DefaultTableModel instances
+    private static DefaultTableModel combineTableModels(DefaultTableModel model1, DefaultTableModel model2) {
+        DefaultTableModel combinedModel = new DefaultTableModel();
+
+        // Add columns from model1 to combinedModel
+        for (int i = 0; i < model1.getColumnCount(); i++) {
+            combinedModel.addColumn(model1.getColumnName(i));
+        }
+
+        // Add columns from model2 to combinedModel (ignoring columns present in model1)
+        for (int i = 0; i < model2.getColumnCount(); i++) {
+            String columnName = model2.getColumnName(i);
+            if (!containsColumn(combinedModel, columnName)) {
+                combinedModel.addColumn(columnName);
+            }
+        }
+
+        // Add rows from model1 and model2 to combinedModel
+        int rowCount = Math.max(model1.getRowCount(), model2.getRowCount());
+        for (int i = 0; i < rowCount; i++) {
+            Vector<Object> rowData = new Vector<>();
+            for (int j = 0; j < combinedModel.getColumnCount(); j++) {
+                if (j < model1.getColumnCount()) {
+                    rowData.add(model1.getValueAt(i, j));
+                } else {
+                    rowData.add(model2.getValueAt(i, j - model1.getColumnCount()));
+                }
+            }
+            combinedModel.addRow(rowData);
+        }
+
+        return combinedModel;
+    }
+
+    // Method to check if a column exists in the DefaultTableModel
+    private static boolean containsColumn(DefaultTableModel model, String columnName) {
+        for (int i = 0; i < model.getColumnCount(); i++) {
+            if (model.getColumnName(i).equals(columnName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
