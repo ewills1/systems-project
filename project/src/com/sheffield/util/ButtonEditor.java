6255f78e5893d91b890f7b014eebeb70d9488944
Afiq Ismail <mabismail1@sheffield.ac.uk>
2023-11-29 05:49:42 +0000

    Code Modification: Implemented "Add to OrderLine" function "Remove" from OrderLine function

--- project/src/com/sheffield/util/ButtonEditor.java
+++ project/src/com/sheffield/util/ButtonEditor.java
@@ -5,6 +5,8 @@ import java.awt.Window;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 import javax.swing.DefaultCellEditor;
@@ -16,9 +18,12 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import com.sheffield.views.ItemFormScreen;
+import com.sheffield.views.OrderLineScreen;
 import com.sheffield.views.ProductListingScreen;
+import com.sheffield.views.UserScreen;
 import com.sheffield.model.Product;
 import com.sheffield.model.OrderLine;
+import com.sheffield.model.CurrentUserManager;
 import com.sheffield.model.DatabaseOperations;
 import com.sheffield.model.Order;
 import com.sheffield.model.Status;
@@ -30,18 +35,21 @@ public class ButtonEditor extends DefaultCellEditor {
     private boolean isPushed;
     private JTable table;
     private Connection connection;
-    private String storedId;
+    // Product related private data
+    private String storedProductCode;
     private String storedProductName;
     private String storedBrandName;
     private BigDecimal storedPrice;
     private String storedGauge;
     private int storedQuantity;
-    private int orderID;
-    private List<OrderLine> orderLines;
-    private Date date;
-    private double totalCost;
+    // User related private data
+    private String forename;
+    private String role;
+    // OrderLine related private data
+    private String storedOrderLine;
 
     DatabaseOperations databaseOperations = new DatabaseOperations();
+    java.util.Date utilDate = new java.util.Date();
 
     public ButtonEditor(JTextField textField, JTable table, Connection connection) {
         super(textField);
@@ -52,12 +60,22 @@ public class ButtonEditor extends DefaultCellEditor {
         button.addActionListener(e -> {
             int row = this.table.getEditingRow(); // Get the row index of the clicked button
             int column = 1; // Get the column index of the clicked button
-            storedId = (String) table.getValueAt(row, column);
-            storedProductName = (String) table.getValueAt(row, column + 1);
-            storedBrandName = (String) table.getValueAt(row, column + 2);
-            storedPrice = (BigDecimal) table.getValueAt(row, column + 3);
-            storedQuantity = (int) table.getValueAt(row, column + 4);
-            storedGauge = (String) table.getValueAt(row, column + 5);
+            
+            if (this.label.equals("Update | Delete")) {
+                storedProductCode = (String) table.getValueAt(row, column);
+            } else if (this.label.equals("Promote | Demote")) {
+                forename = (String) table.getValueAt(row, column+1);
+                role = (String) table.getValueAt(row, column + 4);
+            } else if (this.label.equals("Add to OrderLine")) {
+                storedProductCode = (String) table.getValueAt(row, column);
+                storedProductName = (String) table.getValueAt(row, column + 1);
+                storedBrandName = (String) table.getValueAt(row, column + 2);
+                storedPrice = (BigDecimal) table.getValueAt(row, column + 3);
+                storedQuantity = (int) table.getValueAt(row, column + 4);
+                storedGauge = (String) table.getValueAt(row, column + 5);
+            } else if (this.label.equals("Remove")) {
+                storedOrderLine = (String) table.getValueAt(row, column);
+            }
             // Perform different actions based on row and column indices
             fireEditingStopped();
         });
@@ -73,8 +91,8 @@ public class ButtonEditor extends DefaultCellEditor {
     public Object getCellEditorValue() {
         if (isPushed) {
             // Perform the action when the button is clicked
-            if (this.label == "Update/Delete") {
-                JOptionPane.showMessageDialog(button, "Editing: " + storedId);
+            if (this.label.equals("Update | Delete")) {
+                JOptionPane.showMessageDialog(button, "Editing: " + storedProductCode);
                 // Close the current screen (frame)
                 Window window = SwingUtilities.windowForComponent(button);
                 if (window instanceof JFrame) {
@@ -82,11 +100,46 @@ public class ButtonEditor extends DefaultCellEditor {
                 }
 
                 // Open another screen (frame)
-                JFrame newFrame = new ItemFormScreen(connection, storedId);
+                JFrame newFrame = new ItemFormScreen(connection, storedProductCode);
                 newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Change to your desired close operation
                 // Configure the new frame: set size, add components, etc.
                 newFrame.setVisible(true);
-            } else if (this.label == "Add to order") {
+            } else if (this.label.equals("Promote | Demote")) {
+                String message = "";
+                if (this.role.equals("User")) {
+                    message = "Promote to Staff?";
+                } else if (this.role.equals("Staff")) {
+                    message = "Demote from Staff?";
+                } else {
+                    message = "You're a manager.";
+                }
+
+                int choice = JOptionPane.showConfirmDialog(null, "[" + this.forename + " | " + this.role + "] " + message , "Confirmation", JOptionPane.YES_NO_OPTION);
+                if (choice == JOptionPane.YES_OPTION) {
+                    if (this.role.equals("User")) {
+                        databaseOperations.promoteToStaff(connection, this.forename);
+                        JOptionPane.showMessageDialog(null, this.forename + " has been promoted to Staff.");
+                    } else if (this.role.equals("Staff")) {
+                        databaseOperations.demoteStaff(connection, this.forename);
+                        JOptionPane.showMessageDialog(null, this.forename + " has been demoted to User.");
+                    }
+
+                    // Close the current screen (frame)
+                    Window window = SwingUtilities.windowForComponent(button);
+                    if (window instanceof JFrame) {
+                        window.dispose();
+                    }
+
+                    // Open another screen (frame)
+                    JFrame newFrame = new UserScreen(connection, "");
+                    newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Change to your desired close operation
+                    newFrame.setVisible(true);
+                } else {
+                    System.out.println("User clicked No or closed the dialog.");
+                    // Perform actions for 'No' selection or dialog close
+                }
+
+            } else if (this.label.equals("Add to OrderLine")) {
                 JFrame frame = new JFrame();
                 String inputQuantity = JOptionPane.showInputDialog(frame,
                         "Enter quantity for " + this.storedProductName + " :");
@@ -95,37 +148,73 @@ public class ButtonEditor extends DefaultCellEditor {
                 if (inpQ > storedQuantity) {
                     JOptionPane.showMessageDialog(button, "Input too large");
                 } else {
-                    Product product = new Product(storedId, storedProductName, storedBrandName,
-                            storedQuantity, storedPrice, storedGauge);
-                    // create an orderLine
-                    OrderLine line = new OrderLine(orderID, product, inpQ);
-                    orderLines.add(line);
-                    databaseOperations.insertOrderLine(connection, line);
+                    // creating order if there is no order created from the user
+                    String userIDFirst2Char = CurrentUserManager.getCurrentUser().getUserID();
+                    String orderID = userIDFirst2Char.substring(0, Math.min(userIDFirst2Char.length(), 2)).toUpperCase();
+                    try {
+                        int userOrderCount = databaseOperations.countUserOrder(CurrentUserManager.getCurrentUser().getUserID(), connection);
+                        if (userOrderCount == 0) {
 
-                    /*
-                     * THIS CODE CREATES THE ORDER AND ADDS IT TO THE DATABASE
-                     * CREATE A SEPARATE BUTTON FOR THIS
-                     * 
-                     * Order order = new Order(orderID, orderLines, date, Status.PENDING);
-                     * totalCost = order.getTotalCost();
-                     * 
-                     * databaseOperations.insertOrder(connection, order); // confirm order
-                     * 
-                     * orderID += 1;
-                     */
+                            orderID = orderID + (userOrderCount + 1);
+                            String userID = CurrentUserManager.getCurrentUser().getUserID();
+                            BigDecimal totalCost = new BigDecimal(Double.parseDouble("0"));
+                            Date date = new Date(utilDate.getTime());
 
-                    // implement code that adds the storedProductName and its inputQuantity to the
-                    // order as an
-                    // orderLine
+                            Order order = new Order(orderID, userID, totalCost, Status.PENDING,  date);
+                            databaseOperations.insertOrder(connection, order);
+
+                        } else {
+                            userOrderCount = databaseOperations.countUserOrder(CurrentUserManager.getCurrentUser().getUserID(), connection);
+                            orderID = orderID + userOrderCount;
+                            Order order = databaseOperations.getOrderModel(orderID, connection);
+                            if(order.getStatus().equals(Status.CONFIRMED)) {
+                                orderID = orderID.substring(0, Math.min(orderID.length(), 2));
+                                orderID = orderID + (userOrderCount + 1);
+                                String userID = CurrentUserManager.getCurrentUser().getUserID();
+                                BigDecimal totalCost = new BigDecimal(Double.parseDouble("0"));
+                                Date date = new Date(utilDate.getTime());
+                                order = new Order(orderID, userID, totalCost, Status.PENDING,  date);
+                                databaseOperations.insertOrder(connection, order);
+
+                            }
+                        }
+
+                        // orderLine insert
+                        Product product = databaseOperations.getProductModel(this.storedProductCode, connection);
+                        String userIDLast2Char = CurrentUserManager.getCurrentUser().getUserID();
+                        userIDLast2Char = userIDLast2Char.substring(userIDLast2Char.length() - 2).toUpperCase();
+                        int orderLineCount = databaseOperations.countUserOrderLine((orderID + userIDLast2Char), connection);
+                        String orderLineNumber = orderID + userIDLast2Char + (orderLineCount + 1);
+                        
+                        // System.out.println("ORDERLINE: " + orderLineNumber);
+                        // System.out.println("PRODUCTCODE: " + product.getProductCode());
+                        // System.out.println("ORDERID: " + orderID);
+                        // System.out.println("QUANTITY: " + inpQ);
+                        // System.out.println("ORDERLINECOST: " + product.getPrice());
+                        BigDecimal orderLineCost = product.getPrice().multiply(new BigDecimal(Double.parseDouble(inputQuantity)));
+                        OrderLine orderLine = new OrderLine(orderLineNumber, product.getProductCode(), orderID, inpQ, orderLineCost);
+                        databaseOperations.insertOrderLine(connection, orderLine);
+
+                    } catch(SQLException e) {
+                        e.getStackTrace();
+                    } catch(ParseException e) {
+                        e.getStackTrace();
+                    }
                     JOptionPane.showMessageDialog(button, "Product added to order");
-                    // Window window = SwingUtilities.windowForComponent(button);
-                    // if (window instanceof JFrame) {
-                    // window.dispose();
-                    // }
-
-                    // JFrame newFrame = new ProductListingScreen(connection, "");
-                    // newFrame.setVisible(true);
                 }
+            } else if (this.label.equals("Remove")) {
+                databaseOperations.deleteOrderLine(connection, storedOrderLine);
+                JOptionPane.showMessageDialog(button, "Product deleted from OrderLine");
+                // Close the current screen (frame)
+                    Window window = SwingUtilities.windowForComponent(button);
+                    if (window instanceof JFrame) {
+                        window.dispose();
+                    }
+
+                    // Open another screen (frame)
+                    JFrame newFrame = new OrderLineScreen(connection, "");
+                    newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Change to your desired close operation
+                    newFrame.setVisible(true);
             }
         }
         isPushed = false;
